584f4f6f59bb5d4a6b9a65e26621a61ef40df1e8
jmp0xf <jmp0xf@gmail.com>
2017-09-16 12:05:42 +0800

    fix wrong hanging indentation

--- jingpai/urls.py
+++ jingpai/urls.py
@@ -32,5 +32,5 @@ if settings.DEBUG:
     import debug_toolbar
 
     urlpatterns = [
-                      url(r'^__debug__/', include(debug_toolbar.urls)),
-                  ] + urlpatterns
+        url(r'^__debug__/', include(debug_toolbar.urls)),
+    ] + urlpatterns
