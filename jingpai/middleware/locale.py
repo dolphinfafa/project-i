da279300628f96f6ff12479fc42f6d8fb8cb0889
jmp0xf <jmp0xf@gmail.com>
2017-09-16 12:05:58 +0800

    remove unused variable

--- jingpai/middleware/locale.py
+++ jingpai/middleware/locale.py
@@ -24,7 +24,7 @@ class LocaleMiddleware(MiddlewareMixin):
 
     def process_request(self, request):
         urlconf = getattr(request, 'urlconf', settings.ROOT_URLCONF)
-        i18n_patterns_used, prefixed_default_language = is_language_prefix_patterns_used(urlconf)
+        i18n_patterns_used, _ = is_language_prefix_patterns_used(urlconf)
         language_from_path = translation.get_language_from_path(request.path_info)
         if not language_from_path and i18n_patterns_used:
             language = translation.get_language_from_request(request, check_path=i18n_patterns_used)
