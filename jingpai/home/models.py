72405fbe8ac75b81b290eb52ca779dad191ac61a
jmp0xf <jmp0xf@gmail.com>
2017-09-20 19:49:33 +0800

    为首页model的字段提供help text以方便编辑

--- jingpai/home/models.py
+++ jingpai/home/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
 from wagtail.wagtailadmin.edit_handlers import FieldPanel
 from wagtail.wagtailcore.fields import RichTextField
 from wagtail.wagtailcore.models import Page
@@ -6,14 +7,15 @@ from wagtail.wagtailimages.edit_handlers import ImageChooserPanel
 
 
 class HomePage(Page):
-    liqueur_intro = RichTextField(null=True)
-    brand_intro = RichTextField(null=True)
+    liqueur_intro = RichTextField(null=True, help_text=_("Introduction for Jing liqueur"))
+    brand_intro = RichTextField(null=True, help_text=_("Introduction for Jing Brand"))
     jumbotron_image = models.ForeignKey(
         'wagtailimages.Image',
         null=True,
         blank=True,
         on_delete=models.SET_NULL,
-        related_name='+'
+        related_name='+',
+        help_text=_("Background image for home jumbotron"),
     )
 
     content_panels = Page.content_panels + [
