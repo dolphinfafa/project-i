2b44756db39ab04489b021f0c9596986305ae8db
jmp0xf <jmp0xf@gmail.com>
2017-09-24 21:48:53 +0800

    使用bootstrap4的utilities class处理nav-brand的padding

--- static/js/bootstrap.js
+++ static/js/bootstrap.js
@@ -1,4 +1,5 @@
 require('bootstrap/dist/css/bootstrap.css')
+require('../css/bootstrap4/utilities.css')
 require('font-awesome/css/font-awesome.css')
 global.jQuery = require('jquery')
 var bootstrap = require('bootstrap')
