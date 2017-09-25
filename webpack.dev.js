25ee2390d54093eaeed9d91934406dec8113378a
jmp0xf <jmp0xf@gmail.com>
2017-09-25 19:26:52 +0800

    引入webpack插件避免重复构建

--- webpack.dev.js
+++ webpack.dev.js
@@ -2,6 +2,7 @@ const merge = require('webpack-merge')
 const common = require('./webpack.common.js')
 const ExtractTextWebpackPlugin = require('extract-text-webpack-plugin')
 const CleanWebpackPlugin = require('clean-webpack-plugin')
+const OnlyIfChangedPlugin = require('only-if-changed-webpack-plugin')
 
 module.exports = merge(common, {
   output: {
@@ -36,3 +37,13 @@ var cleanOptions = {
 }
 
 module.exports.plugins.push(new CleanWebpackPlugin(pathsToClean, cleanOptions))
+
+var opts = {
+  rootDir: process.cwd(),
+  devBuild: process.env.NODE_ENV !== 'production',
+}
+
+module.exports.plugins.push(new OnlyIfChangedPlugin({
+  cacheDirectory: opts.rootDir + '/tmp/cache',
+  cacheIdentifier: opts,  // all variable opts/environment should be used in cache key
+}))
