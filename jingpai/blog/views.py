b530929a78ef0deb8b313d6222d883d83e4e1e8a
jmp0xf <jmp0xf@gmail.com>
2017-09-26 23:41:11 +0800

    为博客首页添加分页器

--- jingpai/blog/views.py
+++ jingpai/blog/views.py
@@ -1,3 +1,4 @@
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import TemplateView
 
 from .models import BlogIndexPage, BlogPostPage
@@ -6,8 +7,20 @@ from .models import BlogIndexPage, BlogPostPage
 class BlogIndexView(TemplateView):
     template_name = "blog/blog_index_page.html"
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
+    def get(self, request, *args, **kwargs):
+        page = request.GET.get('p')
+        post_list = BlogPostPage.objects.live().order_by('-first_published_at').all()
+        paginator = Paginator(post_list, 5)  # Show 5 posts per page
+        try:
+            posts = paginator.page(page)
+        except PageNotAnInteger:
+            # If page is not an integer, deliver first page.
+            posts = paginator.page(1)
+        except EmptyPage:
+            # If page is out of range (e.g. 9999), deliver last page of results.
+            posts = paginator.page(paginator.num_pages)
+
+        context = self.get_context_data(**kwargs)
         context['page'] = BlogIndexPage.objects.first()
-        context['posts'] = BlogPostPage.objects.live().order_by('-first_published_at').all()
-        return context
+        context['posts'] = posts
+        return self.render_to_response(context)
