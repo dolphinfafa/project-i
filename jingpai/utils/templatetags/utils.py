010685da33f4f28ad3e09a8bfb09135d64df3f4a
jmp0xf <jmp0xf@gmail.com>
2017-09-27 10:37:58 +0800

    使翻页链接生成支持原链接带参数

--- jingpai/utils/templatetags/utils.py
+++ jingpai/utils/templatetags/utils.py
@@ -13,3 +13,16 @@ def get_locale_setter_url(request, lang):
     params = request.GET.copy()
     params['locale'] = lang
     return f'{request.path}?{params.urlencode()}'
+
+
+@register.filter
+def get_pager_url(request, page):
+    params = request.GET.copy()
+    if page == 1:
+        del params['page']
+    else:
+        params['page'] = page
+    if params:
+        return f'{request.path}?{params.urlencode()}'
+    else:
+        return request.path
